a0986934f83da6e84543a9c070d37b83e68cfd89
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-03 19:11:56 -0500

    feat: implements the other methods

--- src/router/router.js
+++ src/router/router.js
@@ -16,6 +16,18 @@ export default class MyRouter {
         this.router.get(path, this.generateCustomResponses, this.applyCallbacks(callbacks))
     }
 
+    post(path, ...callbacks) {
+        this.router.post(path, this.generateCustomResponses, this.applyCallbacks(callbacks))
+    }
+
+    put(path, ...callbacks) {
+        this.router.put(path, this.generateCustomResponses, this.applyCallbacks(callbacks))
+    }
+
+    delete(path, ...callbacks) {
+        this.router.delete(path, this.generateCustomResponses, this.applyCallbacks(callbacks))
+    }
+
     applyCallbacks(callbacks) {
         return callbacks.map(item => async(...params) => {
             try {
@@ -29,6 +41,8 @@ export default class MyRouter {
 
     generateCustomResponses(req, res, next) {
         res.sendSuccess = payload => res.send({ status: "success", payload })
+        res.sendServerError = error => res.status(500).send({ status: "error", error })
+        res.sendUserError = error => res.status(400).send({ status: "error", error })
         next()
     }
 }
--- src/router/user.routes.js
+++ src/router/user.routes.js
@@ -5,5 +5,10 @@ export default class UserRouter extends Router {
         this.get('/', (req, res) => {
             res.sendSuccess('Hola Coders')
         })
+
+        this.post('/:word', (req, res) => {
+            if (req.params.word == 'x') res.sendUserError('No se puede agregar esta palabra')
+            else res.sendSuccess('Word added')
+        })
     }
 }
